2cad4315581aa00c54a2210320df1aa54d36440e
Monisha Krothapalli <mkrothapalli123@gmail.com>
2024-12-05 00:44:20 -0500

    fixed scroll sideways on art

--- personal/src/pages/Resume/Resume.jsx
+++ personal/src/pages/Resume/Resume.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import "./Resume.css"
 import { Background , Nav, Carasoul} from '../../components';
-import {art1, art2, art4, art6, art7, art8, art9, art10, art11, art12, art13} from "../../assets"
 
 const Resume = () => {
   console.log('resume'); 
